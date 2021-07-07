b5f67d9e051ade2f9c57756c22d8d7e99421bf68
Rodion Sychev <rsychev@sfxdx.me>
2021-07-07 20:21:55 +0300

    ended 1.2 tasks

--- ch1/echo3/main.go
+++ ch1/echo3/main.go
@@ -14,7 +14,31 @@ import (
 
 //!+
 func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	fmt.Println(EchoEx1(os.Args))
+	fmt.Println(EchoEx2(os.Args))
 }
 
 //!-
+
+/*
+	ex from book
+*/
+func EchoEx1(args []string) string {
+	return strings.Join(args, " ")
+}
+
+func EchoEx2(args []string) string {
+	res := ""
+	for i := 0; i < len(args); i++ {
+		res += fmt.Sprintf("%v %s \n", i, args[i])
+	}
+	return  res
+}
+
+func echoEx3Concat(args []string) {
+
+}
+
+func echoEx3Join(args []string) {
+
+}
---
+++ ch1/echo3/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkEchoEx1(b *testing.B) {
+	ta := make([]string, 100000)
+	for i:=0; i < b.N;i++{
+		EchoEx1(ta)
+	}
+}
+
+func BenchmarkEchoEx2(b *testing.B) {
+	ta := make([]string, 100000)
+	for i:=0; i < b.N;i++{
+		EchoEx2(ta)
+	}
+}
